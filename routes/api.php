d7f4f4c90ae337ede517f8c5e29461667b44b42e
Davi Pradipta <davipradipta456@gmail.com>
2025-06-30 17:56:09 +0700

    make api product and category

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\CategoryController;
+use App\Http\Controllers\api\ProductController;
+
+
+Route::apiResource('/categories',CategoryController::class)->only('index');
+Route::apiResource('/products',ProductController::class)->only('index','show');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
