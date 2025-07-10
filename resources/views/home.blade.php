41eb8bc246584adcb97ca4973b9bf3dae523ffd1
Davi Pradipta <davipradipta456@gmail.com>
2025-07-11 01:25:30 +0700

    fix tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,15 @@
-<p style="color: aqua;" class="text-5xl text-red-700">halmana</p>
+<!-- resources/views/welcome.blade.php -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel Tailwind</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="">
+    <div class="font-extrabold  text-5xl text-yellow-500 justify-center">
+        Hello World!
+    </div>
+</body>
+</html>
